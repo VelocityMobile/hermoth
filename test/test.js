b8f98a7417b6427d093b57b33cbff1975872f971
Mario Mendes <mariomendes@Velocitys-MacBook-Pro.local>
2017-08-22 12:19:14 +0100

    starts adding tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,29 @@
 /* eslint-env node*/
 
 import assert from 'assert'
+import { map } from 'ramda'
 import sinon from 'sinon'
-import log from '../lib/menna'
+import { init, publish, consume, subscribe } from './../lib/hermoth'
 
 describe('hermoth', () => {
+    describe('connection established', () => {
 
-})
+        it('initiates', async () => {
+            let result = await init()
+            assert.ok(result)
+        });
+
+        it('publishes', async () => {
+            let result = await publish('Test Name', {"Somethin Real": "Minions"})
+            assert.ok(result)
+        });
+
+        // it('consumes', async() => {
+        //     subscribe()
+        //     let name = "Test Name"
+        //     let blob = JSON.parse('{"Somethin Real": "Minions"}')
+        //     let result = await consume({content: JSON.stringify({name, blob})})
+        //     assert.ok(result)
+        // });
+    });
+});
